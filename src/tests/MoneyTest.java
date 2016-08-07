28ce2c88197b22230dfae4df1a752e0e230f23a5
Gabriel Brito <gabriel.brito55@gmail.com>
2016-08-07 14:44:35 -0300

    Fixture criada (4)

--- src/tests/MoneyTest.java
+++ src/tests/MoneyTest.java
@@ -3,13 +3,19 @@ package tests;
 import lab1_junit.Money;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MoneyTest {
-
+	private Money m1;
+	
+	@Before
+	public void setup(){
+		m1 = new Money(10, "BRL");
+	}
+	
 	@Test
 	public void whenAddingBRLExpectSumBRL() {
-		Money m1 = new Money(10, "BRL");
 		Money m2 = new Money(7, "BRL");
 		assertTrue(m1.getCurrency().equals(m2.getCurrency()));
 		m1 = m1.add(m2);
